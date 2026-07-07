1d6004280382477915ee3ca5f0506fad2877c26d
LucaasHenrique <lucashsouza101@gmail.com>
2026-07-07 14:40:13 -0300

    feat: workflow

--- key_pair.tf
+++ key_pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "instance_key" {
   key_name   = "site-server-key"
-  public_key = file("~/.ssh/id_ed25519.pub")
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICevJEgnu5UuQDbWatXYQFnZuwtcMZl0NUH7QZRliE6x lucas@pc"
 }
