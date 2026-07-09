acfbcb04b5b16e1e1a72cdb92bc6e075050c3528
LucaasHenrique <lucashsouza101@gmail.com>
2026-07-09 12:56:10 -0300

    fix: fix the wrong ssh key

--- security_group.tf
+++ security_group.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "website_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.website_sg.id
-  cidr_ipv4         = "45.7.25.24/32"
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
