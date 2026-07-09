4755e2899414b4c85b8a4ae3253aea4a624a2abc
LucaasHenrique <lucashsouza101@gmail.com>
2026-07-09 12:24:22 -0300

    fix: fix the wrong ssh key

--- key_pair.tf
+++ key_pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "instance_key" {
   key_name   = "site-server-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICevJEgnu5UuQDbWatXYQFnZuwtcMZl0NUH7QZRliE6x lucas@pc"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGk59g2lywk7uKO6D2hD0nWEJJi27Q5mau0Nx9JURaHd lucass@fedora"
 }
